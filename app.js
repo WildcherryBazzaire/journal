bc66e47b3b9452d3fa28a4e02e8af80eb9dcbc3c
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2017-03-15 09:10:30 -1000

    added app.js file

---
+++ app.js
@@ -0,0 +1,24 @@
+/*var jsonDataBody = [
+	{
+		"div":{
+			"class": 
+		}
+	}
+];
+*/
+var simplifiedJSON = [
+	{
+		"jorunalEntry": "1",
+
+		"date": "Feb 27, 2017",
+
+		"title": "Why I wanted to take this class",
+
+		"content": "I wanted to take this class because I love the unlimited things you program. Possibilities are endless when you put your mind in the right place. It is also stunning to create code that do exactly what you want it to do.",
+
+		"article": [
+			"title": "What I Expect",
+			"content": "I expect what everyone else in this class expects, to learn code and to due cool-ass projects."
+		]
+	}
+]
